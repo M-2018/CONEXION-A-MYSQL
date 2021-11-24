d588820e3bec2be839898eaa57f6d4c441691ebd
Marco Gonzalez <37376975+M-2018@users.noreply.github.com>
2021-11-23 23:06:07 -0500

    Add files via upload

---
+++ CRUD/CRUD/frmConexion/frmConexion.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace CRUD.FormConexion
+{
+    partial class frmConexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(100, 25);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 1;
+            this.txtServidor.Text = "localhost";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(100, 51);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(100, 20);
+            this.txtPuerto.TabIndex = 3;
+            this.txtPuerto.Text = "3306";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puerto";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(100, 77);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 5;
+            this.txtUsuario.Text = "root";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuario";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(100, 103);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(100, 20);
+            this.txtContrasena.TabIndex = 7;
+            this.txtContrasena.Text = "aranea";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contraseña";
+            // 
+            // txtBd
+            // 
+            this.txtBd.Location = new System.Drawing.Point(100, 129);
+            this.txtBd.Name = "txtBd";
+            this.txtBd.Size = new System.Drawing.Size(100, 20);
+            this.txtBd.TabIndex = 9;
+            this.txtBd.Text = "mysql";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "bd";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(73, 168);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 12;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // frmConexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 211);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtBd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.label1);
+            this.Name = "frmConexion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conexión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
---
+++ CRUD/CRUD/frmConexion/frmConexion.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD.FormConexion
+{
+    public partial class frmConexion : Form
+    {
+        public frmConexion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+
+            //Ejemplo para conectarse  a una db mysql mediante un formulario
+
+            String servidor = txtServidor.Text;
+            String puerto = txtPuerto.Text;
+            String usuario = txtUsuario.Text;
+            String password = txtContrasena.Text;
+            String bd = txtBd.Text;
+
+            string cadenaConexion = "Server =" + servidor + "; Database =" + bd + "; Uid = " + usuario + "; Pwd =" + password;
+
+            MySqlConnection conexionBD = new MySqlConnection(cadenaConexion); //Se crea una nueva instancia de MySqlConnection
+            MySqlDataReader reader = null; //Variable que nos ayuda a leer el resultado de nuestra consulta
+            String data = null;
+
+            try
+            {
+                String consulta = "SHOW DATABASES";
+                MySqlCommand comando = new MySqlCommand(consulta);//MySqlCommand Representa una instrucción SQL para ejecutar en una base de datos MySQL.
+                comando.Connection = conexionBD;
+                conexionBD.Open();
+                reader = comando.ExecuteReader();//ExecuteReader() Sends the CommandText to the Connection and builds a SqlDataReader.
+
+                while (reader.Read())
+                {
+                    data += reader.GetString(0) + "\n";// GetString Returns the Recordset as a string, la posicion inicial es cero
+                }
+                MessageBox.Show(data);
+                frmProductos productos = new frmProductos();
+                productos.Show();
+                this.Hide();
+
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error:" + ex.Message);
+            }
+            finally
+            {
+                conexionBD.Close();
+            }
+        }
+    }
+}
